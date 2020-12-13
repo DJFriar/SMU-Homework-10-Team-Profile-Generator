3087d02d592fc3635608afdd6e2b1b5d436f13d0
DJFriar <tommy.craft@icloud.com>
2020-12-12 21:20:26 -0600

    removed console.log refs, added comments

--- README.md
+++ README.md
@@ -1,8 +1,18 @@
 # SMU-Homework-10-Team-Profile-Generator
 
-SMU Homework 10 - Team Profile Generator
+A command line driven tool that asks the user for details about a team and then generates an HTML page containing the provided details.
 
+## Installation
+
+Download the repo and run `npm install` from repo root. Then use `node app.js` to start the application.
+
+## Usage
+
+A video of the app in action can be [found on YouTube](www.youtube.com)
 ## Credits
 
 - How to use Inquirer.js [Mat Wilmot](https://medium.com/javascript-in-plain-english/how-to-inquirer-js-c10a4e05ef1f)
 
+## License
+
+![license](https://img.shields.io/badge/license-MIT-brightgreen)
--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/htmlRenderer");
 
+// Ask questions realting to the team manager
 const askManager = () => {
   inquirer
     .prompt([
@@ -36,115 +37,110 @@ const askManager = () => {
     .then((res) => {
       const manager = new Manager(res.managerName, res.managerId, res.managerEmail, res.managerOfficeNum)
       team.push(manager);
-      console.log(manager);
       askWhatRole();
     });
 };
 
-  const askWhatRole = () => {
-    inquirer.prompt([
-      {
-        type: 'list',
-        name: 'newEmployeeType',
-        message: 'Which type of team member would you like to add this this team?',
-        choices: ["Engineer", "Intern", "I don't want to add anyone else"]
-      },
-    ])
-    .then((res) => {
-      switch (res.newEmployeeType) {
-        case "Engineer":
-          askEngineer();
-          break;
-        case "Intern":
-          askIntern();
-          break;
-        default:
-          generatePage();
-          break;
-      }
-    });
-  };
+// Routine to add another employee or trigger the page creation routine
+const askWhatRole = () => {
+  inquirer.prompt([
+    {
+      type: 'list',
+      name: 'newEmployeeType',
+      message: 'Which type of team member would you like to add this this team?',
+      choices: ["Engineer", "Intern", "I don't want to add anyone else"]
+    },
+  ])
+  .then((res) => {
+    switch (res.newEmployeeType) {
+      case "Engineer":
+        askEngineer();
+        break;
+      case "Intern":
+        askIntern();
+        break;
+      default:
+        generatePage();
+        break;
+    }
+  });
+};
 
-  const askEngineer = () => {
-    inquirer.prompt([
-      {
-        type: 'input',
-        message: 'What is your Engineer\'s name?',
-        name: 'engineerName',
-      },
-      {
-        type: 'input',
-        message: 'What is your Engineer\'s id?',
-        name: 'engineerId',
-      },
-      {
-        type: 'input',
-        message: 'What is your Engineer\'s email address?',
-        name: 'engineerEmail',
-      },
-      {
-        type: 'input',
-        message: 'What is your Engineer\'s GitHub username?',
-        name: 'engineerGitHub',
-      },
-    ])
-    .then((res) => {
-      const engineer = new Engineer(res.engineerName, res.engineerId, res.engineerEmail, res.engineerGitHub)
-      team.push(engineer);
-      console.log(engineer);
-      askWhatRole();
-    });
-  };
+// Ask the engineer related questions
+const askEngineer = () => {
+  inquirer.prompt([
+    {
+      type: 'input',
+      message: 'What is your Engineer\'s name?',
+      name: 'engineerName',
+    },
+    {
+      type: 'input',
+      message: 'What is your Engineer\'s id?',
+      name: 'engineerId',
+    },
+    {
+      type: 'input',
+      message: 'What is your Engineer\'s email address?',
+      name: 'engineerEmail',
+    },
+    {
+      type: 'input',
+      message: 'What is your Engineer\'s GitHub username?',
+      name: 'engineerGitHub',
+    },
+  ])
+  .then((res) => {
+    const engineer = new Engineer(res.engineerName, res.engineerId, res.engineerEmail, res.engineerGitHub)
+    team.push(engineer);
+    askWhatRole();
+  });
+};
 
-  const askIntern = () => {
-    inquirer.prompt([
-      {
-        type: 'input',
-        message: 'What is your Intern\'s name?',
-        name: 'internName',
-      },
-      {
-        type: 'input',
-        message: 'What is your Intern\'s id?',
-        name: 'internId',
-      },
-      {
-        type: 'input',
-        message: 'What is your Intern\'s email address?',
-        name: 'internEmail',
-      },
-      {
-        type: 'input',
-        message: 'What school did your Intern attend?',
-        name: 'internSchool',
-      },
-    ])
-    .then((res) => {
-      const intern = new Intern(res.internName, res.internId, res.internEmail, res.internSchool)
-      team.push(intern);
-      console.log(intern);
-      askWhatRole();
-    });
-  };
+// Ask the intern related questions
+const askIntern = () => {
+  inquirer.prompt([
+    {
+      type: 'input',
+      message: 'What is your Intern\'s name?',
+      name: 'internName',
+    },
+    {
+      type: 'input',
+      message: 'What is your Intern\'s id?',
+      name: 'internId',
+    },
+    {
+      type: 'input',
+      message: 'What is your Intern\'s email address?',
+      name: 'internEmail',
+    },
+    {
+      type: 'input',
+      message: 'What school did your Intern attend?',
+      name: 'internSchool',
+    },
+  ])
+  .then((res) => {
+    const intern = new Intern(res.internName, res.internId, res.internEmail, res.internSchool)
+    team.push(intern);
+    askWhatRole();
+  });
+};
 
+// gathers the info provided and creates HTML content containing it
 const generatePage = () => {
-  console.log(team);
   const generatedHTML = render(team);
   createTeamPage(generatedHTML);
 }
 
+// function that takes the generated HTML content and writes it to the output folder as an HTML file.
 const createTeamPage = (htmlContent) => {
   fs.writeFile(outputPath, htmlContent, "utf-8", (err) => {
     if(err) throw err;
-    console.log("Team page has been created successfully.");
+    console.log("Team page has been created successfully. You will find it in the output folder.");
   })
 }
 
 // Start the app by asking about the manager
 askManager()
-// .then(() => {
-//   render(team);
-// })
-// .catch((err) => {
-//   console.log(err);
-// });
